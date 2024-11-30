19f03e26dafc123db18174580eac7e44bd648e4e
Vadim31x <vad.vorotnikov@gmail.com>
2024-11-30 13:42:50 +0300

    Update and rename PZ_5.py to PZ_5.1.py

---
+++ PZ_5.1.py
@@ -0,0 +1,9 @@
+# Составить функцию, которая выполнит суммирования числового ряда.
+
+while True:
+    try:
+        x = int(input("Введите послнднее число x: "))
+        print(sum([i for i in range(1, x + 1)]))
+    except ValueError:
+        print("Ошибка")
+    break
